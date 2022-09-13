47885637a680956d98a6cec36361219dbe82956a
Obinna <umeh.ob@gmail.com>
2022-09-13 15:39:50 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Function to check letter case
+ * @c: is the int used
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
