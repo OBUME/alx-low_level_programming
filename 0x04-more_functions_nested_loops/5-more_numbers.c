b2209f335c954418dc48be54d26ebcf6a176e1f0
Obinna <umeh.ob@gmail.com>
2022-09-16 15:52:04 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - prints numbers to 14
- *
- * Return: returns nothing
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 
 void more_numbers(void)
