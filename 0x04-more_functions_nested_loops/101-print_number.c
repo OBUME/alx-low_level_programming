804b5fefc36a3fcbd5d9a2a632d93b280b27fb7f
Obinna <umeh.ob@gmail.com>
2022-09-16 18:08:23 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,49 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: random integer
- * Return: void
+ * print_number -prints a number
+ * @n: the number to print
  */
 
 void print_number(int n)
 {
-	char ld, cr;
-	int r;
-	int ct = 0;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar ('-');
-		ld = (char) ('0' - (n % 10);
-				n = n / -10;
-				}
-
-				else
-				{
-				ld = (char) ((n % 10) + '0');
-				n = n / 10;
-				}
-
-				r = 0;
-				while (n > 0)
-				{
-				r = r * 10 + (n % 10);
-				n = n / 10;
-				ct++;
-				}
-
-				while (r > 0)
-				{
-				cr = (char) ((r % 10) + '0');
-				_putchar (cr);
-				r = r / 10;
-				ct--;
-				}
-				while (ct != 0)
-				{
-					_putchar ('0');
-					ct--;
-				}
-				_putchar (ld);
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar (num % 10 + '0');
 }
